ee61efa89fe3865bf1d9b903dd74ece3fd4c93b4
Jespinalcorrea1 <160746528+Jespinalcorrea1@users.noreply.github.com>
2024-03-15 12:52:48 +0100

    c

--- yatzy.js
+++ yatzy.js
@@ -1,9 +1,23 @@
+function calcularPuntuacion() {
+    // Puedes agregar aquí la lógica JavaScript para calcular la puntuación total.
+    // Por ejemplo, actualiza la columna 'Total' con la suma de todas las puntuaciones.
+     // Obtener los valores de los inputs
+
 var score11 = Number(document.getElementById("score11").value);
 var score12 = Number(document.getElementById("score12").value);
 var score13 = Number(document.getElementById("score13").value);
 var score14 = Number(document.getElementById("score14").value);
 var score15 = Number(document.getElementById("score15").value);
 var score16 = Number(document.getElementById("score16").value);
+var score17 = Number(document.getElementById("score17").value);
+var score172 = Number(document.getElementById("score172").value);
+var score18 = Number(document.getElementById("score18").value);
+var score19 = Number(document.getElementById("score19").value);
+var score110 = Number(document.getElementById("score110").value);
+var score111 = Number(document.getElementById("score111").value);
+var score112 = Number(document.getElementById("score112").value);
+var score113 = Number(document.getElementById("score113").value);
+var score114 = Number(document.getElementById("score114").value);
 
 var score21 = Number(document.getElementById("score21").value);
 var score22 = Number(document.getElementById("score22").value);
@@ -11,6 +25,15 @@ var score23 = Number(document.getElementById("score23").value);
 var score24 = Number(document.getElementById("score24").value);
 var score25 = Number(document.getElementById("score25").value);
 var score26 = Number(document.getElementById("score26").value);
+var score27 = Number(document.getElementById("score27").value);
+var score272 = Number(document.getElementById("score272").value);
+var score28 = Number(document.getElementById("score28").value);
+var score29 = Number(document.getElementById("score29").value);
+var score210 = Number(document.getElementById("score210").value);
+var score211 = Number(document.getElementById("score211").value);
+var score212 = Number(document.getElementById("score212").value);
+var score213 = Number(document.getElementById("score213").value);
+var score214 = Number(document.getElementById("score214").value);
 
 var score31 = Number(document.getElementById("score31").value);
 var score32 = Number(document.getElementById("score32").value);
@@ -18,6 +41,15 @@ var score33 = Number(document.getElementById("score33").value);
 var score34 = Number(document.getElementById("score34").value);
 var score35 = Number(document.getElementById("score35").value);
 var score36 = Number(document.getElementById("score36").value);
+var score37 = Number(document.getElementById("score37").value);
+var score372 = Number(document.getElementById("score372").value);
+var score38 = Number(document.getElementById("score38").value);
+var score39 = Number(document.getElementById("score39").value);
+var score310 = Number(document.getElementById("score310").value);
+var score311 = Number(document.getElementById("score311").value);
+var score312 = Number(document.getElementById("score312").value);
+var score313 = Number(document.getElementById("score313").value);
+var score314 = Number(document.getElementById("score314").value);
 
 var score41 = Number(document.getElementById("score41").value);
 var score42 = Number(document.getElementById("score42").value);
@@ -25,6 +57,15 @@ var score43 = Number(document.getElementById("score43").value);
 var score44 = Number(document.getElementById("score44").value);
 var score45 = Number(document.getElementById("score45").value);
 var score46 = Number(document.getElementById("score46").value);
+var score47 = Number(document.getElementById("score47").value);
+var score472 = Number(document.getElementById("score472").value);
+var score48 = Number(document.getElementById("score48").value);
+var score49 = Number(document.getElementById("score49").value);
+var score410 = Number(document.getElementById("score410").value);
+var score411 = Number(document.getElementById("score411").value);
+var score412 = Number(document.getElementById("score412").value);
+var score413 = Number(document.getElementById("score413").value);
+var score414 = Number(document.getElementById("score414").value);
 
 var score51 = Number(document.getElementById("score51").value);
 var score52 = Number(document.getElementById("score52").value);
@@ -32,6 +73,15 @@ var score53 = Number(document.getElementById("score53").value);
 var score54 = Number(document.getElementById("score54").value);
 var score55 = Number(document.getElementById("score55").value);
 var score56 = Number(document.getElementById("score56").value);
+var score57 = Number(document.getElementById("score57").value);
+var score572 = Number(document.getElementById("score572").value);
+var score58 = Number(document.getElementById("score58").value);
+var score59 = Number(document.getElementById("score59").value);
+var score510 = Number(document.getElementById("score510").value);
+var score511 = Number(document.getElementById("score511").value);
+var score512 = Number(document.getElementById("score512").value);
+var score513 = Number(document.getElementById("score513").value);
+var score514 = Number(document.getElementById("score514").value);
 
 var score61 = Number(document.getElementById("score61").value);
 var score62 = Number(document.getElementById("score62").value);
@@ -39,6 +89,16 @@ var score63 = Number(document.getElementById("score63").value);
 var score64 = Number(document.getElementById("score64").value);
 var score65 = Number(document.getElementById("score65").value);
 var score66 = Number(document.getElementById("score66").value);
+var score67 = Number(document.getElementById("score67").value);
+var score672 = Number(document.getElementById("score672").value);
+var score68 = Number(document.getElementById("score68").value);
+var score69 = Number(document.getElementById("score69").value);
+var score610 = Number(document.getElementById("score610").value);
+var score611 = Number(document.getElementById("score611").value);
+var score612 = Number(document.getElementById("score612").value);
+var score613 = Number(document.getElementById("score613").value);
+var score614 = Number(document.getElementById("score614").value);
+
 
 // Agrega más variables para los demás números
 // Calcular el total de la primera fila
@@ -68,80 +128,6 @@ var bonus5 = total5 >= 63 ? 25 : 0;
 document.getElementById("bonus5").value = bonus5;
 var bonus6 = total6 >= 63 ? 25 : 0;
 document.getElementById("bonus6").value = bonus6;
-
-function calcularPuntuacion() {
-    // Puedes agregar aquí la lógica JavaScript para calcular la puntuación total.
-    // Por ejemplo, actualiza la columna 'Total' con la suma de todas las puntuaciones.
-     // Obtener los valores de los inputs
-
-
-var score17 = Number(document.getElementById("score17").value);
-var score172 = Number(document.getElementById("score172").value);
-var score18 = Number(document.getElementById("score18").value);
-var score19 = Number(document.getElementById("score19").value);
-var score110 = Number(document.getElementById("score110").value);
-var score111 = Number(document.getElementById("score111").value);
-var score112 = Number(document.getElementById("score112").value);
-var score113 = Number(document.getElementById("score113").value);
-var score114 = Number(document.getElementById("score114").value);
-
-
-var score27 = Number(document.getElementById("score27").value);
-var score272 = Number(document.getElementById("score272").value);
-var score28 = Number(document.getElementById("score28").value);
-var score29 = Number(document.getElementById("score29").value);
-var score210 = Number(document.getElementById("score210").value);
-var score211 = Number(document.getElementById("score211").value);
-var score212 = Number(document.getElementById("score212").value);
-var score213 = Number(document.getElementById("score213").value);
-var score214 = Number(document.getElementById("score214").value);
-
-
-var score37 = Number(document.getElementById("score37").value);
-var score372 = Number(document.getElementById("score372").value);
-var score38 = Number(document.getElementById("score38").value);
-var score39 = Number(document.getElementById("score39").value);
-var score310 = Number(document.getElementById("score310").value);
-var score311 = Number(document.getElementById("score311").value);
-var score312 = Number(document.getElementById("score312").value);
-var score313 = Number(document.getElementById("score313").value);
-var score314 = Number(document.getElementById("score314").value);
-
-
-var score47 = Number(document.getElementById("score47").value);
-var score472 = Number(document.getElementById("score472").value);
-var score48 = Number(document.getElementById("score48").value);
-var score49 = Number(document.getElementById("score49").value);
-var score410 = Number(document.getElementById("score410").value);
-var score411 = Number(document.getElementById("score411").value);
-var score412 = Number(document.getElementById("score412").value);
-var score413 = Number(document.getElementById("score413").value);
-var score414 = Number(document.getElementById("score414").value);
-
-
-var score57 = Number(document.getElementById("score57").value);
-var score572 = Number(document.getElementById("score572").value);
-var score58 = Number(document.getElementById("score58").value);
-var score59 = Number(document.getElementById("score59").value);
-var score510 = Number(document.getElementById("score510").value);
-var score511 = Number(document.getElementById("score511").value);
-var score512 = Number(document.getElementById("score512").value);
-var score513 = Number(document.getElementById("score513").value);
-var score514 = Number(document.getElementById("score514").value);
-
-
-var score67 = Number(document.getElementById("score67").value);
-var score672 = Number(document.getElementById("score672").value);
-var score68 = Number(document.getElementById("score68").value);
-var score69 = Number(document.getElementById("score69").value);
-var score610 = Number(document.getElementById("score610").value);
-var score611 = Number(document.getElementById("score611").value);
-var score612 = Number(document.getElementById("score612").value);
-var score613 = Number(document.getElementById("score613").value);
-var score614 = Number(document.getElementById("score614").value);
-
-
-
 
 
 // Sumar todos los valores de los inputs y mostrar el resultado en el último input deshabilitado
